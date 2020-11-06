2e5f200b8e610bfa8e2487dcc90c848e690bc4e4
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-11-06 15:01:18 +0530

    initial commit

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>PhotoGallery</h1>
+      <h2>Your Images</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+        asperiores, culpa, corrupti, eos consectetur assumenda consequuntur
+        vitae distinctio quia quam qui id quidem alias! Illum suscipit
+        reprehenderit nemo voluptate sapiente.
+      </p>
+    </div>
+  );
+}
+
+export default Title;
