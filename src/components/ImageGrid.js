d1806f139131647e2f79b9f0fa097f1c21659e4c
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2020-11-06 22:33:19 +0530

    add modal add logic for opening and closing of modal

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,7 +1,7 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
 
-function ImageGrid() {
+function ImageGrid({ setSelectedImage }) {
   const { docs } = useFirestore("images");
 
   return (
@@ -9,7 +9,11 @@ function ImageGrid() {
       {docs &&
         docs.map((doc) => {
           return (
-            <div className="img-wrap" key={doc.id}>
+            <div
+              className="img-wrap"
+              key={doc.id}
+              onClick={() => setSelectedImage(doc.url)}
+            >
               <img src={doc.url} alt="coming soon..." />
             </div>
           );
